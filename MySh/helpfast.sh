7d9430702f54fe8044ea81e2f46964250c206368
andy <2369050467@qq.com>
2024-09-02 13:47:22 +0800

    helpfast.sh update

--- MySh/helpfast.sh
+++ MySh/helpfast.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #文件夹下替换某一格式文件中多个字符串脚本
 #替换的内容由一个文件输入，文件字符串以分隔符|分割作为替换内容
-
+set -x
 echo "欢迎使用帮助字符串快速替换文本脚本，此文本在英文单词量少时可能会有问题，其他特殊情况也会有，请小心使用，需要输入一个源文件，模版每行格式为："
 echo "语言|被替换字串|替换字串"
 echo "语言为文件夹名称字串"
@@ -38,13 +38,18 @@ do
     else
         echo "folder do not exists "
     fi
-    #if [ $format -eq "htm"]; then
+
+    #if [[ $format = "htm" ]]; then
         #替换帮助文档字串
         #cd $SPATH/$lan.lproj/WebHelp/
+        #echo "format 为 htm"
     #else
         #替换UI字串
         #cd $SPATH/$lan.lproj/
+        #echo "format 为1 $format"
     #fi
+
+    #格式匹配
     case $format in
         "htm")
             cd $SPATH/$lan.lproj/WebHelp/
